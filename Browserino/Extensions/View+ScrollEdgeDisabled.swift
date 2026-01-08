79b3d0b9b044cb13f906305a706b1e1c30a37472
Aleksandr <a.strizhnev@urbi.ae>
2026-01-08 12:34:04 +0400

    fix: revert scrolledge hack

--- Browserino/Extensions/View+ScrollEdgeDisabled.swift
+++ Browserino/Extensions/View+ScrollEdgeDisabled.swift
@@ -10,8 +10,10 @@ import SwiftUI
 extension View {
     @ViewBuilder
     func scrollEdgeEffectDisabledCompat() -> some View {
-        if #available(macOS 15.0, *) {
-            self.scrollClipDisabled()
+        if #available(macOS 26.0, *) {
+            self
+                .scrollEdgeEffectStyle(.soft, for: .all)
+                .scrollEdgeEffectDisabled(true, for: .all)
         } else {
             self
         }
